3590984cc00145b982b0f5140108ac0d8e8e6694
z1lV3r <zilver.thd@gmail.com>
2025-03-12 14:49:11 -0600

    Refactor packages names

---
+++ src/test/java/dsa/leetcode/_0704/binary/search/SolutionTest.java
@@ -0,0 +1,33 @@
+package dsa.leetcode._0704.binary.search;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+public class SolutionTest {
+
+    private final Solution solution = new Solution();
+    private int[] nums;
+    private int target;
+
+    @Test
+    public void case1() {
+        nums = new int[]{-1,0,3,5,9,12};
+        target = 9;
+
+        int result = solution.search(nums, target);
+        assertEquals(4, result);
+    }
+
+    @Test
+    public void case2() {
+        nums = new int[]{-1,0,3,5,9,12};
+        target = 2;
+
+        int result = solution.search(nums, target);
+        assertEquals(-1, result);
+    }
+}
